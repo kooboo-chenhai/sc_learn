8c87c0d5014c8da000b536eb28bcf5c6908a0bfc
chenhai-kooboo <chenhai@kooboo.cn>
2018-02-02 19:13:22 +0800

    fix tac.utils speed

---
+++ Areas/Importer/Controllers/EventsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Areas.Importer.Controllers
+{
+    public class EventsController: Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFile file, string parentPath)
+        {
+            //IEnumerable<Event> events = null;
+            //string message = null;
+            //using (var reader = new System.IO.StreamReader(file.InputStream))
+            //{
+            //    var contents = reader.ReadToEnd();
+            //    try
+            //    {
+            //        events = JsonConvert
+            //    }
+            //}
+                return View();
+        }
+    }
+}
---
+++ Areas/Importer/Models/Event.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Areas.Importer.Models
+{
+    public class Event
+    {
+        public Event() { }
+        public string ContentHeading
+        {
+            get;
+            set;
+        }
+        public string ContentIntro
+        {
+            get;
+            set;
+        }
+        public string Highlights
+        {
+            get;
+            set;
+        }
+        public string StartDate
+        {
+            get;
+            set;
+        }
+        public string Duration
+        {
+            get;
+            set;
+        }
+        public string Difficulty
+        {
+            get;
+            set;
+        }
+    }
+}
