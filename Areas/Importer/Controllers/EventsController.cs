9f53bd0a17c56f7b46498ca2232448e8b3e6511a
chenhai-kooboo <chenhai@kooboo.cn>
2018-02-05 14:11:25 +0800

    course 9 without comment form

--- Areas/Importer/Controllers/EventsController.cs
+++ Areas/Importer/Controllers/EventsController.cs
@@ -1,31 +1,60 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Areas.Importer.Models;
 
 namespace Website.Areas.Importer.Controllers
 {
     public class EventsController: Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+
         [HttpPost]
-        public ActionResult Index(HttpPostedFile file, string parentPath)
+        public ActionResult Index(HttpPostedFileBase file, string parentPath)
         {
-            //IEnumerable<Event> events = null;
-            //string message = null;
-            //using (var reader = new System.IO.StreamReader(file.InputStream))
-            //{
-            //    var contents = reader.ReadToEnd();
-            //    try
-            //    {
-            //        events = JsonConvert
-            //    }
-            //}
-                return View();
+            IEnumerable<Event> events = null;
+            string message = null;
+            using (var reader = new System.IO.StreamReader(file.InputStream))
+            {
+                var contents = reader.ReadToEnd();
+                try
+                {
+                    events = JsonConvert.DeserializeObject<IEnumerable<Event>>(contents);
+                }
+                catch (Exception e)
+                {
+                    // to be added later
+                }
+            }
+
+
+            var database = Sitecore.Configuration.Factory.GetDatabase("master");
+            var parentItem = database.GetItem(parentPath);
+            var templateId = new TemplateID(new ID("{81316562-3773-43F0-A4B6-7E632C25665B}"));
+            using (new SecurityDisabler())
+            {
+                foreach (var ev in events)
+                {
+                    var name = ItemUtil.ProposeValidItemName(ev.ContentHeading);
+                    Item item = parentItem.Add(name, templateId);
+                    item.Editing.BeginEdit();
+                    item["ContentHeading"] = ev.ContentHeading;
+                    //set value to all other fields
+                    item.Editing.EndEdit();
+                }
+            }
+            return View();
         }
     }
 }
---
+++ Scripts/commentsForm.js
@@ -0,0 +1,18 @@
+﻿function createCommentItem(form, path) {
+debugger
+    var service = new ItemService({ url: '/sitecore/api/ssc/item' });
+    var obj = {
+        ItemName: 'comment - ' + form.name.value,
+        TemplateID: '{17730FFD-14FB-44B1-ABEC-A3B6A60F1A7C}',
+        Name: form.name.value,
+        Comment: form.comment.value
+    };
+    sevice.create(obj).path(path).execute().then(function (item) {
+        form.name.value = form.comment.value = '';
+        window.alert('Thanks. Your message will show on the site shortly')
+    }).fail(function (err) {
+        window.alert(err);
+        });
+    event.preventDefault();
+    return false;
+}
