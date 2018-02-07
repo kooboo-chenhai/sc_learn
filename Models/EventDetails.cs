1a9955d021a1ed2dc08aaabcba19bdc912308065
chenhai-kooboo <chenhai@kooboo.cn>
2018-02-07 21:07:23 +0800

    11 api done

---
+++ Models/EventDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Web.UI.WebControls;
+
+namespace Website.Models
+{
+    public class EventDetails : SearchResultItem
+    {
+        public string ContentHeading
+        {
+            get;
+            set;
+        }
+        public string ContentIntro
+        {
+            get;
+            set;
+        }
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+        public HtmlString EventImage
+        {
+            get {
+                return new HtmlString(FieldRenderer.Render(GetItem(), "Event Image", "DisableWebEditing=true&mw=150"));
+            }
+        }
+    }
+}
---
+++ Models/EventsList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class EventsList
+    {
+        public IEnumerable<EventDetails> Events
+        {
+            get;
+            set;
+        }
+        public int TotalResultCount
+        {
+            get;
+            set;
+        }
+        public int PageSize
+        {
+            get;
+            set;
+        }
+    }
+}
